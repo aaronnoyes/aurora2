8f61b15023276b585e46803d6043a5ffbea68b6f
noyesa@myumanitoba.ca <aaronnoyes@Aarons-MacBook-Pro.local>
2018-11-20 16:10:38 -0600

    started search result expansion

--- js/search.js
+++ js/search.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
   var workingCourseList = fullWinterCourseList;
   var searchTerm = "";
   var re = new RegExp('.*');
-  var activeResult = "";
+  var activeResult = null;
 
 
   //hot udpate search filter
@@ -92,7 +92,10 @@ $(document).ready(function(){
       $('#course-list').append('<div class="search-result" id="' + courseList[i].courseID +'">' +
       '<h5 class="search-result-title">' + courseList[i].courseID + ': </h5>' +
       courseList[i].name +
-      '<div class"expanded-placeholder"></div>' +
+      '<div class="expanded-placeholder">' +
+      courseList[i].department + '</br>' +
+      courseList[i].credits + '</br>' +
+      '</div>' +
       '<button class="view-button">View' +
       '<img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAh0lEQVQ4T93TMQrCUAzG8V9x8QziiYSuXdzFC7h4AcELOPQAdXYovZCHEATlgQV5GFTe1ozJlz/kS1IpjKqw3wQBVyy++JI0y1GTe7DCBbMAckeNIQKk/BanALBB+16LtnDELoMcsM/BESDlz2heDR3WePwKSLo5eoxz3z6NNcFD+vu3ij14Aqz/DxGbKB7CAAAAAElFTkSuQmCC"></img>' +
       '</button>' +
@@ -108,11 +111,18 @@ $(document).ready(function(){
 
     // Re-add event handler for all matching elements
     $(".view-button").on("click", function() {
-      var parentID = $(this).closest('div').attr('id');
-      var selectedCourse = workingCourseList.find(function(element) {
-        return element.courseID == parentID;
-      });
-      console.log(selectedCourse);
+      // var parentID = $(this).closest('div').attr('id');
+      // var selectedCourse = workingCourseList.find(function(element) {
+      //   return element.courseID == parentID;
+      // });
+      // console.log(selectedCourse);
+      console.log($(this).siblings()[1]);
+      if(activeResult != null) {
+        $(activeResult).removeClass("active");
+      }
+      activeResult = $(this).siblings()[1];
+      console.log(activeResult);
+      $(activeResult).addClass("active");
     });
   }
 
