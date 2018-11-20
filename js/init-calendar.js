883bc50a9936c2be80b61030d02b866fc26ca296
Daniel Lovegrove <d.lovegrove11@gmail.com>
2018-11-20 16:00:55 -0600

    Make calendar fade in when bringing it up

--- js/init-calendar.js
+++ js/init-calendar.js
@@ -2,7 +2,7 @@
 // Display the calendar with courses on document ready
 //
 
-$(document).ready(function() {
+$(document).ready(function () {
     $('#calendar-schedule').fullCalendar({
         header: {
             left: 'month,agendaWeek',
@@ -12,8 +12,23 @@ $(document).ready(function() {
         height: 800,
         defaultView: 'agendaWeek',
         events: createFullCalendarEventsForSchedule(studentData),
-        eventClick: function(event) {
+        eventClick: function (event) {
             alert(event.title + " Information: Not available!");
+        }
+    })
+
+    $('#pills-schedule-tab').tab('show')
+
+    //hide the schedule when other tabs are active
+    $('a[data-toggle="pill"]').on('show.bs.tab', function (e) {
+        e.target; // newly activated tab
+        e.relatedTarget; // previous active tab
+
+        if (e.target.id != "pills-schedule-tab") {
+            $("#calendar-schedule").hide();
+        } else {
+            console.log("show calendar")
+            $("#calendar-schedule").fadeIn(500);
         }
     })
 });
