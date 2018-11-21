99f37ed3cf99c3b5a4cede5d9d768a336691ce6f
noyesa@myumanitoba.ca <aaronnoyes@Aarons-MacBook-Pro.local>
2018-11-21 03:25:12 -0600

    added (non-functional) course registration

--- js/search.js
+++ js/search.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
   var fullFallCourseList = fall2018Courses.courses;
   var fullWinterCourseList = winter2019Courses.courses;
   var workingCourseList = fullWinterCourseList;
+  var curTerm = "WINTER2019";
   var searchTerm = "";
   var re = new RegExp('.*');
   var buttonIdRegex = new RegExp(/BUTTON\-/)
@@ -23,10 +24,12 @@ $(document).ready(function(){
     if ($(this).val() == "w2019") {
       appendCoursesToList(fullWinterCourseList);
       workingCourseList = fullWinterCourseList;
+      curTerm = "WINTER2019";
     }
     else {
       appendCoursesToList(fullFallCourseList);
       workingCourseList = fullFallCourseList;
+      curTerm = "FALL2018";
     }
 
     filterCourses();
@@ -35,7 +38,6 @@ $(document).ready(function(){
   //changed course level
   $("#level-select").change(function () {
 
-    console.log($(this).val());
     var level = $(this).val()[0];
     // var lastFive = id.substr(id.length - 5);
 
@@ -123,9 +125,10 @@ $(document).ready(function(){
 
   //generate the info for each section in course
   function generateSectionsDivs(sections, id) {
-    var sectionsDiv = ""
+    var sectionsDiv = "";
     for(var i=0; i<sections.length; i++) {
       sectionsDiv += generateSectionDiv(sections[i], id);
+      first = false;
     }
     return sectionsDiv;
   }
@@ -135,23 +138,57 @@ $(document).ready(function(){
     return `
     <div id="${id}-section-${section.section}" class="section-div">
       <span>Days: ${section.days}</span>
+      <br/>
       <span>Instructor: ${section.instructor}</span>
+      <br/>
       <span>Location: ${section.location}</span>
+      <br/>
+      <button class="register-button" id="${id}-register-btn-${section.section}">Register</button>
     </div>`
+  }
+
+  //show only section chosen from dropdown
+  $("ul#course-list").on('change', ".section-select", function(e) {
+    var parentID = $(this).parent().attr('id').split("-")[1];
+    var idToShow = `${parentID}-section-${$(this).val()}`;
+    var sectionDivs = $(this).siblings("div");
+
+    for(var i=0; i<sectionDivs.length; i++) {
+      if(idToShow == sectionDivs[i].id) {
+        sectionDivs[i].style.display = "";
+      }
+      else {
+        sectionDivs[i].style.display = "none";
+      }
+    }
+  });
+
+  function initSectionSelectVisibility(id) {
+    var first = true;
+    sectionDivs = $(`#${id}-section-select`).siblings("div");
+
+    for(var i=0; i<sectionDivs.length; i++) {
+      if(first) sectionDivs[i].style.display = "";
+      else sectionDivs[i].style.display = "none";
+      first = false;
+    }
+
   }
 
   // Handles buttons being clicked
   $('ul#course-list').on('click', function(e) {
-    //put space back in id for retreiving from course list
-    var id = e.target.id.split("-")[1];
-    var idWithSpace = id.replace(/([A-Z]+)([1-9]+)/g, '$1 $2');
-    var sections = workingCourseList.find(function(element) {
-      return element.courseID == idWithSpace;
-    }).sections;
-    console.log(sections);
+    // console.log(e.target.id.split("-"))
 
-
+    //hide/show button clicked
     if (buttonIdRegex.test(e.target.id)) {
+
+      //put space back in id for retreiving from course list
+      var id = e.target.id.split("-")[1];
+      var idWithSpace = id.replace(/([A-Z]+)([1-9]+)/g, '$1 $2');
+      var sections = workingCourseList.find(function(element) {
+        return element.courseID == idWithSpace;
+      }).sections;
+
       var dropdownIdSelector = `#${e.target.id.replace('BUTTON-', 'DROPDOWN-')}`;
 
       if(activeDropdown != null) {
@@ -166,10 +203,38 @@ $(document).ready(function(){
         $(activeDropdown).addClass("active");
         $(activeDropdown).siblings("button").html("Hide");
         $(activeDropdown).append(generateSectionsDivs(sections, id));
+        initSectionSelectVisibility(id);
       }
       else {
         activeDropdown = null;
       }
+    }
+
+    //register button clicked
+    else if (e.target.id.split("-")[1] == "register") {
+      var student = studentData;
+      var cID = e.target.id.split("-")[0];
+      var cIDWithSpace = cID.replace(/([A-Z]+)([1-9]+)/g, '$1 $2');
+      var course = workingCourseList.find(function(element) {
+        return element.courseID == cIDWithSpace;
+      });
+
+      var section = course.sections.find(function(element) {
+        return element.section == e.target.id.split("-")[3];
+      });
+      var formattedCourse = {
+          name: course.name,
+          courseID: course.courseID,
+          days: section.days,
+          term: curTerm,
+          section: section.section,
+          time: {
+              start: section.time.start,
+              end: section.time.end
+          }
+      };
+      console.log(formattedCourse);
+      addCourseToSchedule(student, formattedCourse);
     }
   });
 
